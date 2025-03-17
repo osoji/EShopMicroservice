b685615a86548628865821196e93312a80d0585f
Odetunde Adesoji <adesojiodetunde@gmail.com>
2025-03-17 12:11:23 +0100

    Add docker

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,15 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
+builder.Services.AddCarter(); //for routing the minimal api
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
-app.MapCarter();
+app.MapCarter(); 
 
 app.Run();
